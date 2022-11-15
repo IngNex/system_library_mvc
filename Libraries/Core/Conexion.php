e70dc9f0bc602def292424e424b80439eb740524
IngNex <ingnexmaicol14@gmail.com>
2022-11-15 13:38:09 -0500

    connection data base

--- Libraries/Core/Conexion.php
+++ Libraries/Core/Conexion.php
@@ -3,14 +3,14 @@ class Conexion{
     private $conect;
     public function __construct()
     {
-      $conexion = "mysql:host=".HOST.";dbname=".BD.";.CHARSET.";  
-      try {
-          $this->conect = new PDO($conexion, DB_USER, PASS);
-          $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch (PDOException $e) {
-          $this->conect = "Error en la conexion";
-          echo "Error: " . $e->getMessage();
-      }
+    $conexion = "mysql:host=".HOST.";dbname=".BD.";.CHARSET.";  
+        try {
+            $this->conect = new PDO($conexion, DB_USER, PASS);
+            $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $this->conect = "Error en la conexion";
+            echo "Error: " . $e->getMessage();
+        }
     }
     public function conect()
     {
